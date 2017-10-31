6fb0d9f3bfa395f2cd6187421242d5de7788891d
Stephen Arnsparger <stephen.arnsparger>
2017-10-30 23:24:25 -0500

    Did basic setup for nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static("public");
+
+// Routes
+app.get('/', function(request, response) {
+  response.render('index.ejs');
+});
+
+// Listener
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
