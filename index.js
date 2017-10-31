8826e5d23f93fe27ccc82d5f88e536a23146b55b
Stephen Arnsparger <stephen.arnsparger>
2017-10-31 08:34:37 -0500

    Fixed the path errors with Bootstrap

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var app = express();
-var api_caller = require('javascript/github_api_manager')
+var express = require('express')
+var app = express()
+// var api_caller = require('javascript/github_api_manager')
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static("public"));
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static("public"))
+app.use('/twbs', express.static(__dirname + '/node_modules/bootstrap/dist/'))
 
 // Routes
 app.get('/', function(request, response) {
-  response.render('index.ejs');
-});
+  response.render('index.ejs')
+})
 
 // Listener
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+  console.log('Node app is running on port', app.get('port'))
+})
