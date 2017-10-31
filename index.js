8cf2cacf45898da75fecf1ca9c4861064520c937
Stephen Arnsparger <stephen.arnsparger>
2017-10-30 23:38:58 -0500

    Fixed a syntax error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static("public");
+app.use(express.static("public"));
 
 // Routes
 app.get('/', function(request, response) {
